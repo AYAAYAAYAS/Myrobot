b54d8ead595196875535cb8e7b597f54ac00b130
Aya <qq2287829073@gmail.com>
2024-05-17 15:04:56 +0800

    1

---
+++ control-motor/USER/Inc/encode.h
@@ -0,0 +1,11 @@
+#ifndef __ENCODER_H
+#define __ENCODER_H
+
+#include "main.h"
+
+int Read_Encoder(uint8_t TIMX);
+float Moto_Speed(int encoder_cnt,uint16_t ppr,uint16_t ratio,uint16_t cnt_time);
+long Num_Encoder_Cnt(float num,uint16_t ppr,float ratio);
+long Rpm_Encoder_Cnt(float rpm,uint16_t ppr,uint16_t ratio,uint16_t cnt_time);
+
+#endif
