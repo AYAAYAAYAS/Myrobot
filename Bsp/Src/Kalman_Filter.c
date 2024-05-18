651d07a844c5228d11b3adc3730cc297d480079b
Aya <145241159+AYAAYAAYAS@users.noreply.github.com>
2024-05-18 10:15:02 +0800

    Create  Kalman_Filter.c

---
+++ Bsp/Src/Kalman_Filter.c
@@ -0,0 +1,28 @@
+void Kalman_Filter(float angle_m,float gyro_m)            //gyro_m:gyro_measure
+{
+    angle+=(gyro_m-q_bias) * dt;//先验估计
+    Pdot[0]=Q_angle - P[0][1] - P[1][0];// Pk-' 先验估计误差协方差的微分
+    Pdot[1]=- P[1][1];
+    Pdot[2]=- P[1][1];
+    Pdot[3]=Q_gyro;
+    P[0][0] += Pdot[0] * dt;// Pk- 先验估计误差协方差微分的积分 = 先验估计误差协方差
+    P[0][1] += Pdot[1] * dt;
+    P[1][0] += Pdot[2] * dt;
+    P[1][1] += Pdot[3] * dt;
+    angle_err = angle_m - angle;//zk-先验估计
+    PCt_0 = C_0 * P[0][0];
+    PCt_1 = C_0 * P[1][0];
+    E = R_angle + C_0 * PCt_0;
+    K_0 = PCt_0 / E;//Kk
+    K_1 = PCt_1 / E;
+    t_0 = PCt_0;
+    t_1 = C_0 * P[0][1];
+
+    P[0][0] -= K_0 * t_0;//后验估计误差协方差
+    P[0][1] -= K_0 * t_1;
+    P[1][0] -= K_1 * t_0;
+    P[1][1] -= K_1 * t_1;
+    angle    += K_0 * angle_err;//后验估计
+    q_bias    += K_1 * angle_err;//后验估计
+    angle_dot = gyro_m-q_bias;//输出值（后验估计）的微分 = 角速度
+}
