87be9b903694d70d8dfecc1c4feeab33d6453525
Aya <145241159+AYAAYAAYAS@users.noreply.github.com>
2024-05-18 08:57:23 +0800

    Update trace.py
    增加了openmv的通信，创建了一个数据帧，用于与stm32通信

--- openmv/trace.py
+++ openmv/trace.py
@@ -1,4 +1,7 @@
-import sensor, image, time, math
+import sensor, image, time, math, ustruct
+from pyb import UART
+uart = UART(3,115200)#初始化串口3，波特率为115200
+uart.init(115200 ,bits = 8 ,parity = None ,stop = 1)#停止位8位，优先级无，停止位1
 # 追踪黑线。使用 [(128, 255)] 追踪白线.
 GRAYSCALE_THRESHOLD = [(0, 64)]
 # 下面是一个 roi【区域】元组列表。每个 roi 用 (x, y, w, h)表示的矩形。
@@ -16,10 +19,23 @@ for r in ROIS: weight_sum += r[4] # r[4] 为矩形权重值.
 sensor.reset() # 初始化.
 sensor.set_pixformat(sensor.GRAYSCALE) # 使用灰度图像.
 sensor.set_framesize(sensor.QQVGA) # 使用 QQVGA 分辨率（160*120）.
-sensor.skip_frames(time = 2000) # 等待摄像头稳定.
+sensor.skip_frames(time = 1000) # 等待摄像头稳定.
 sensor.set_auto_gain(False) # 必须关闭自动增益
 sensor.set_auto_whitebal(False) # 必须关闭白平衡
 clock = time.clock() # Tracks FPS.
+
+def Sending_data():
+    global uart;
+    data=ustruct.pack("<bbhhhhb",
+                      0x2C,
+                      0x12,
+                      int(buffer_1),
+                      int(buffer_2),
+                      int(buffer_3),
+                      0x5B)
+   uart.write(data);
+
+
 while(True):
     clock.tick() # Track elapsed milliseconds between snapshots().
     img = sensor.snapshot() # 实时拍照.
@@ -36,14 +52,20 @@ while(True):
     centroid_sum += largest_blob.cx() * r[4]
     center_pos = (centroid_sum / weight_sum) # 确定直线的中心.
  # 将直线中心位置转换成角度，便于机器人处理.
-    deflection_angle = 0
+    deflection_volue = 0
  # 使用反正切函数计算直线中心偏离角度。可以自行画图理解
 #权重 X 坐标落在图像左半部分记作正偏，落在右边部分记为负偏，
 #所以计算结果加负号。
-    deflection_angle = -math.atan((center_pos-80)/60)
+    deflection_volue = -math.atan((center_pos-80)/60)
  # 将偏离值转换成偏离角度.
-    deflection_angle = math.degrees(deflection_angle)
+    deflection_angle = math.degrees(deflection_volue)
 
+    buffer_1=center_pos;
+    buffer_2=deflection_volue;
+    buffer_3=deflection_angle
+    OUT_DATA =bytearray([0x2C,0x12,buffer_1, buffer_2,  buffer_3, 0x5B ] #发送数据包
+    uart.write(OUT_DATA)
  # 计算偏离角度后可以控制机器人进行调整.
+    print(OUT_DATA)
     print("Turn Angle: %f" % deflection_angle)
     print(clock.fps())
